b89e8bfcef63fc9b5bff8e1e7601320a48dacef3
Devin Ward <devinw1325wc@gmail.com>
2021-03-05 11:54:52 -0600

    Welcome controller and bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "../stylesheets/application"
+import "bootstrap"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
