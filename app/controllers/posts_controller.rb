402cdf5e1a699ecf21d24b53e43f6b9c606b416e
Devin Ward <devinw1325wc@gmail.com>
2021-03-11 15:35:10 -0600

    add posts to profile, deletion, and post index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,33 @@
 class PostsController < ApplicationController
 before_action :authenticate_user!
 
-    def index
-        @posts = Post.all.order(created_at: :desc)
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def new
+    @post = current_user.posts.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to posts_path, notice: "Created Post"
+    else
+      render :new, notice: "Please try again"
     end
+  end
 
-    def new
-        @post = current_user.posts.new
-    end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to user_path(@post.user.username), notice: "Post deleted"
+  end
 
-    def create
-        @post = current_user.posts.build(post_params)
+  private
 
-        if @post.save
-            redirect_to posts_path, notice: "Created Post"
-        else
-            render :new, notice: "Please try again"
-        end
-    end
-
-    private
-
-    def post_params
-        params.require(:post).permit(:image)
-    end
+  def post_params
+    params.require(:post).permit(:image)
+  end
 end
